83d6624ecdf16561449efcb17b8e4f14f048d0c3
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-17 16:27:42 -0500

    Update __manifest__.py

--- administrative_expenses/__manifest__.py
+++ administrative_expenses/__manifest__.py
@@ -6,7 +6,11 @@
         due date""",
 
     'description': """
-        Add new line in subscription line ids
+        When a customer pays late, 
+        is in arrears or the service is blocked, 
+        a penalty charge is added to the following month's bill.
+        Since every month there are several clients to whom these charges have to be added, 
+        they could be added massively so that they would later appear automatically in the next invoice that is issued in subscriptions.
     """,
 
     'version': '13.0.1.0',
