c213e8da88521650dce43d1e64cd57558b387336
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-17 12:25:02 -0500

    Update res_partner.py

--- administrative_expenses/models/res_partner.py
+++ administrative_expenses/models/res_partner.py
@@ -4,4 +4,11 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_blocking = fields.Boolean(
-        string="Blocking")
+        string="Blocking",
+        compute="_compute_check_category_id")
+
+
+    @api.depends('category_id')
+    def _compute_check_category_id(self):
+        for record in self:
+            record.is_blocking = True if record.category_id and record.category_id[0].name  == 'BLOQUEO' else False
