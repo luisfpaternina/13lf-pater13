a479be8ee73a9f4191eaa449f2911150eee50d2e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-24 12:54:23 -0500

    Update product.py

--- stock_ext/models/product.py
+++ stock_ext/models/product.py
@@ -7,6 +7,15 @@ class ProductProduct(models.Model):
     codigo_ncm = fields.Char(string="Codigo NCM")
 
 
+    @api.onchange('codigo_ncm'):
+    def _onchange_ncm(self):
+        product_obj = self.env['product.template'].search([('name', '=', self.name)],limit=1)
+        if product_obj:
+            self.codigo_ncm = self.product_obj.codigo_ncm
+        else:
+            self.codigo_ncm = False
+
+
 
 
 
@@ -14,3 +23,12 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     codigo_ncm = fields.Char(string="Codigo NCM")
+
+
+    @api.onchange('codigo_ncm'):
+    def _onchange_ncm(self):
+        product_obj = self.env['product.product'].search([('name', '=', self.name)],limit=1)
+        if product_obj:
+            self.codigo_ncm = self.product_obj.codigo_ncm
+        else:
+            self.codigo_ncm = False
