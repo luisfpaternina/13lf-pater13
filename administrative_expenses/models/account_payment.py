5a12084d3b1f2c298b0ab9bfe3769e93f54128cd
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-27 18:27:31 -0500

    Update account_payment.py

--- administrative_expenses/models/account_payment.py
+++ administrative_expenses/models/account_payment.py
@@ -5,20 +5,23 @@ import logging
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
+    
     def post(self):
         rec = super(AccountPayment, self).post()
         acc_move_obj = self.env['account.move'].search([('name','=',self.communication)],limit=1)
         if acc_move_obj:
             for record in acc_move_obj:
                 if record.invoice_payments_widget:
+                    date_str = record.invoice_payments_widget.split()[23].replace('"',"")
+                    date_str = date_str.replace(',','')
                     logging.info("widget dateeeeeeeeeeeeeeeeeee", record.invoice_payments_widget)
                     logging.info("ENTROOOOOOOOOOOOOOOOOOOO CAUCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", record.invoice_payments_widget)
-                    date_str = record.invoice_payments_widget
                     logging.info("date strrrrrrrrrrrrrrrr",date_str)
-                    date_dt = datetime.strptime(date_str, '%d/%m/%Y')
+                    date_dt = datetime.strptime(date_str, '%Y-%m-%d')
                     logging.info("kkkkkkkkkkkkkkkkkkkkkkkkkkkk", date_dt)
                     date_date = date_dt.date()
                     record.register_date = date_date
                     logging.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
                     logging.info(record.register_date)
         return rec
+    
