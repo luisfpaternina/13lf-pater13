de7d9e0f2f38287fc00da53455df02970b0bf6c7
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-27 10:02:37 -0500

    Update sale_subscription.py

--- administrative_expenses/models/sale_subscription.py
+++ administrative_expenses/models/sale_subscription.py
@@ -6,9 +6,3 @@ from pytz import timezone
 
 class SaleSuscriptionInherit(models.Model):
     _inherit = 'sale.subscription'
-
-    def _recurring_create_invoice(self):
-        res = super(SaleSuscriptionInherit, self)._recurring_create_invoice()
-        for record in self:
-            record._validate_subscription()
-        return res
