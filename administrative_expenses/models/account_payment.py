35269ee8310c6db195554b27b525dbac1cf3636e
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-27 16:59:13 -0500

    Update account_payment.py

--- administrative_expenses/models/account_payment.py
+++ administrative_expenses/models/account_payment.py
@@ -2,5 +2,10 @@ from odoo import models, fields, api, _
 from datetime import datetime, date
 import logging
 
-class AccountPayment(models.TransientModel):
+class AccountPayment(models.Model):
     _inherit = 'account.payment'
+
+    def create(self, vals):
+        rec = super(AccountJournal, self).create(vals)
+        # ...        
+        return rec
