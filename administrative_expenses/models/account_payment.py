e395cd72b3969c8d69a3288dcc8264378fd0b324
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-27 17:16:18 -0500

    Update account_payment.py

--- administrative_expenses/models/account_payment.py
+++ administrative_expenses/models/account_payment.py
@@ -5,7 +5,19 @@ import logging
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    def create(self, vals):
-        rec = super(AccountJournal, self).create(vals)
-        # ...        
+    def post(self):
+        rec = super(AccountPayment, self).post()
+        acc_move_obj = self.env['account.move'].search([('name','=',self.communication)],limit=1)
+        if acc_move_obj:
+            for record in acc_move_obj:
+                if record.invoice_payments_widget:
+                    logging.info("ENTROOOOOOOOOOOOOOOOOOOO AL TEXTTTTTTTTTTTTTTTTTTTTTTTT")
+                    date_str = record.invoice_payments_widget
+                    logging.info("date strrrrrrrrrrrrrrrr",date_str)
+                    date_dt = datetime.strptime(date_str, '%d-%m-%Y')
+                    logging.info("kkkkkkkkkkkkkkkkkkkkkkkkkkkk", date_dt)
+                    date_date = date_dt.date()
+                    record.register_date = date_date
+                    logging.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                    logging.info(record.register_date)
         return rec
