7143de67868f019d9debd054744b6ce19e1d562a
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-01-30 08:52:21 -0500

    Update res_config_settings.py

--- administrative_expenses/models/res_config_settings.py
+++ administrative_expenses/models/res_config_settings.py
@@ -52,9 +52,11 @@ class ResConfigSettings(models.TransientModel):
         return {
             'late_charge': conf.get_param('admministrative_expenses.late_charge'),
             'late_fee': conf.get_param('admministrative_expenses.late_fee'),
+            'late_charge_value': conf.get_param('admministrative_expenses.late_charge_value'),
         }
 
     def set_expenses_values(self):
         conf = self.env['ir.config_parameter']
         conf.set_param('admministrative_expenses.late_charge', self.late_charge)
         conf.set_param('admministrative_expenses.late_fee', self.late_fee)
+        conf.set_param('admministrative_expenses.late_fee', self.late_charge_value)
